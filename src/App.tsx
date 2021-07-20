68131bcfc8dc35cc6a9528a0534fb43e98d8be3a
Kostya <ttes7a@gmial.com>
2021-07-20 13:21:53 +0300

    Second

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import {Todolist} from "./components/Todolist";
+
+function App() {
+
+    const tasks1 = [
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "ReactJS", isDone: false},
+    ]
+
+    const tasks2 = [
+        {id: 1, title: "Hello world", isDone: true},
+        {id: 2, title: "I am Happy", isDone: false},
+        {id: 3, title: "Yo", isDone: false},
+    ]
+
+    return (
+        <div className="App">
+            <Todolist title="What to learn" tasks={tasks1}/>
+            <Todolist title="Songs" tasks={tasks2}/>
+            {/*<Todolist title="Books"/>*/}
+        </div>
+    );
+}
+
+export default App;
